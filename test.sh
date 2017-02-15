867a0fc71783419fd29cefaf9ebc180a94a35dbf
Sebastian Kreutzberger <s.kreutzberger@googlemail.com>
2017-02-15 15:05:27 +0100

    added test.sh for running tests in Docker container

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# -e: exit when a command fails
+# -o pipefail: set exit status of shell script to last nonzero exit code, if any were nonzero.
+set -o pipefail
+
+echo ""
+echo "Running Tests in Docker Container"
+echo "================================="
+docker build -t aes256cbc .
+docker run --name aes256cbc_test --rm aes256cbc swift test
+echo "Finished, all containers were removed."
