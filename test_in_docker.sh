051bb439421ba6a11b1180fa010ab66eb10b7db8
Laurent Gaches <laurent@binimo.com>
2017-08-05 09:30:03 -0500

    Swift 4 and 3 compatibility

--- test_in_docker.sh
+++ test_in_docker.sh
@@ -5,8 +5,14 @@
 set -o pipefail
 
 echo ""
-echo "Running Tests in Docker Container"
+echo "Running Swift 3.1 Tests in Docker Container"
 echo "================================="
 docker build -t aes256cbc .
 docker run --name aes256cbc_test --rm aes256cbc swift test
+echo "Finished tests, docker container were removed."
+echo ""
+echo "Running Swift 4.0 Tests in Docker Container"
+echo "================================="
+docker build -t aes256cbc-swift4 -f Dockerfile~swift4 .
+docker run --name aes256cbc_test --rm aes256cbc-swift4 swift test
 echo "Finished tests, docker container were removed."
